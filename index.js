180f307f588a39f11ee2f367c41b9c908e62dc59
jtreeves <jackson.t.reeves@gmail.com>
2020-12-15 16:30:07 -0500

    [index] Create blog post

--- index.js
+++ index.js
@@ -9,10 +9,25 @@ mongoose.connect('mongodb://localhost/mongooseAssociation')
 
 const db = mongoose.connection
 
+db.once('open', () => {
+    console.log(`Connected to MongoDB on ${db.host}: ${db.port}`)
+})
+
+db.on('error', (err) => {
+    console.log(`Error: ${err}`)
+})
+
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.send('Home Route, Backend')
+})
+
+app.get('/blog', (req, res) => {
+    BlogPost.create({
+        title: 'Mongoose for all Mongoose',
+        body: 'This is a cool blog post'
+    })
 })
 
 const PORT = process.env.PORT || 3000
